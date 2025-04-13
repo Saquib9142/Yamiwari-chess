b958af57f5c7c870c032190f178fb37e8c891faf
Saquib9142 <saquibalam366@gmail.com>
2025-04-13 18:34:45 +0530

    style.css

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let selectedPiece = null;
+body {
+  font-family: sans-serif;
+  text-align: center;
+}
 
-document.querySelectorAll('.square').forEach(square => {
-    square.addEventListener('click', () => {
-        if (selectedPiece) {
-            // Move piece
-            square.innerHTML = selectedPiece.innerHTML;
-            selectedPiece.innerHTML = '';
-            selectedPiece = null;
-        } else if (square.innerHTML !== '') {
-            // Select piece
-            selectedPiece = square;
-        }
-    });
-});
+#chessboard {
+  display: grid;
+  grid-template-columns: repeat(8, 60px);
+  grid-template-rows: repeat(8, 60px);
+  margin: 20px auto;
+  width: 480px;
+}
+
+.square {
+  width: 60px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 36px;
+  cursor: pointer;
+}
+
+.white { background-color: #f0d9b5; }
+.black { background-color: #b58863; }
